f00fd8b60a45076f84985006eb7c0e3697cbe3dc
jorge.ahumadacastell <jorge.ahumadacastell@CA-L1MZS493.groupinfra.com>
2021-06-27 23:53:32 -0300

    First commit, demo project created

---
+++ Assignment/src/main/java/SearchPage.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+//import pageObjects.SearchPage;
+
+public class SearchPage {
+	
+
+	public static void main(String[] args) throws InterruptedException {
+		//Check TestScenarios folder and run as cucumber feature the Login.Senario.feature file
+		
+	} 
+
+}
